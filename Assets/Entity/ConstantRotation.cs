84c3be8fd1d30876cd190ba2212b5cd6ceecdce9
gbm <gabehilism@gmail.com>
2020-03-22 07:15:04 -0300

    primeira skill do staff

---
+++ Assets/Entity/ConstantRotation.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConstantRotation : MonoBehaviour
+{
+    public Vector3 Rotation;
+    public float Speed = 5f;
+    public float SpeedFactor = 1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Rotation * Speed * SpeedFactor * Time.deltaTime, Space.Self);
+    }
+}
---
+++ Assets/FX/SpawnParticleSystemOnDestroy.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnParticleSystemOnDestroy : MonoBehaviour
+{
+    public GameObject Particles;
+
+    private void OnDestroy()
+    {
+        if (!Application.isPlaying) return;
+        Instantiate(Particles, transform.position, Quaternion.identity);
+    }
+}
