0736f6e8ff116d2ac6ea304620978e7a1d0a7139
gbm <gabehilism@gmail.com>
2020-04-22 19:07:50 -0300

    adicionado uns barris no começo do level

--- Assets/Entity/Interactable/Interactable.cs
+++ Assets/Entity/Interactable/Interactable.cs
@@ -6,22 +6,35 @@ public class Interactable : MonoBehaviour
 
     public System.Action<CharacterData> OnInteract;
 
-    private bool selected;
+    private bool selected { get { return Mathf.Approximately(selectedT, 1f); } }
+    private float selectedT;
+    private float selectedTarget;
 
     private static int hashSelected = Shader.PropertyToID("_Selected");
 
     void OnPlayerInteract(CharacterData player)
     {
-        if (!selected) return;
+        //if (!selected) return;
         OnInteract?.Invoke(player);
+    }
+
+    private void Update()
+    {
+        if (Mathf.Approximately(selectedT, selectedTarget)) return;
+
+        selectedT = Mathf.Lerp(selectedT, selectedTarget, Time.deltaTime * 2f);
+        if (Mathf.Abs(selectedTarget - selectedT) < 0.01f) selectedT = selectedTarget;
+        else
+        {
+            System.Array.ForEach(renderers, renderer => renderer.material.SetFloat(hashSelected, selectedT));
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (!other.CompareTag("Player")) return;
 
-        selected = true;
-        System.Array.ForEach(renderers, renderer => renderer.material.SetFloat(hashSelected, 1f));
+        selectedTarget = 1f;
         other.GetComponent<CharacterPlayerInput>().OnInteract += OnPlayerInteract;
     }
 
@@ -29,8 +42,7 @@ public class Interactable : MonoBehaviour
     {
         if (!other.CompareTag("Player")) return;
 
-        selected = false;
-        System.Array.ForEach(renderers, renderer => renderer.material.SetFloat(hashSelected, 0f));
+        selectedTarget = 0f;
         other.GetComponent<CharacterPlayerInput>().OnInteract -= OnPlayerInteract;
     }
 }
