4bc89c165e05a0f2fbb871d47ddc9be1447eb2af
gbm <gabehilism@gmail.com>
2020-11-25 00:38:52 -0300

    PropPlacement.cs can place props in cell objects

---
+++ Assets/Scripts/Level/Editor/PropPlacementTestEditor.cs
@@ -0,0 +1,14 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(PropPlacementTest))]
+public class PropPlacementTestEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        if (GUILayout.Button("Organize"))
+            (target as PropPlacementTest).OrganizeProps();
+    }
+
+}
---
+++ Assets/Scripts/Level/PropPlacementTest.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Catacumba.LevelGen;
+using System.Linq;
+
+[RequireComponent(typeof(MeshRenderer))]
+public class PropPlacementTest : MonoBehaviour
+{
+    public float percentage = 0.2f;
+    private new MeshRenderer renderer;
+    public EDirectionBitmask directions;
+
+    public Transform[] targetObjects;
+
+    List<PropPlacement.BoundsSize> bounds;
+
+    public void OrganizeProps()
+    {
+        bounds = PropPlacement.OrganizeProps(gameObject, 
+                                             directions, 
+                                             targetObjects.Select(o=>o.gameObject).ToArray(), 
+                                             percentage);
+    }
+
+    void OnDrawGizmos()
+    {
+        if (bounds == null) return;
+        Gizmos.color = Color.yellow;
+        foreach (var b in bounds)
+            Gizmos.DrawWireCube(b.bounds.center, b.bounds.size);
+    }
+}
